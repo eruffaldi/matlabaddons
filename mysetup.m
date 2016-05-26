0dddaa4e5a098e65debe89eaff253e2e5267e002
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-05-26 12:58:51 +0200

    added tprod

--- mysetup.m
+++ mysetup.m
@@ -77,7 +77,8 @@ sgraph = struct('name','graph','folder','toolbox_graph','website','http://it.mat
 surlread2 = struct('name','urlread2','folder','urlread2','website','http://www.mathworks.com/matlabcentral/fileexchange/35693-urlread2','url','http://www.mathworks.com/matlabcentral/mlc-downloads/downloads/submissions/35693/versions/3/download/zip','startup','');
 sprt = struct('name','prt','folder','PRT','website','https://github.com/covartech/PRT','url','https://github.com/covartech/PRT','startup','prtPath');
 spcl = struct('name','pcd','folder','pcd','website','http://www.mathworks.com/matlabcentral/fileexchange/40382-matlab-to-point-cloud-library','url','http://www.mathworks.com/matlabcentral/mlc-downloads/downloads/submissions/40382/versions/6/download/zip','startup','');
-q = [sbtk,seeg,sfig,sekfukf,squat,srosbag,srobot,sbnt,spmtk,sdimple,sgraph,sgpml,snetlab,sjson,sjsonmex,swobj,sgraph,surlread2,sprt,spcl];
+stprod = struct('name','tprod','folder','tprod','website','http://www.mathworks.com/matlabcentral/fileexchange/16275-tprod-arbitary-tensor-products-between-n-d-arrays','url','https://www.mathworks.com/accesslogin/index_fe.do?uri=http%3A%2F%2Fwww.mathworks.com%2Fmatlabcentral%2Ffileexchange%2F16275-tprod-arbitary-tensor-products-between-n-d-arrays','startup','');
+q = [sbtk,seeg,sfig,sekfukf,squat,srosbag,srobot,sbnt,spmtk,sdimple,sgraph,sgpml,snetlab,sjson,sjsonmex,swobj,sgraph,surlread2,sprt,spcl,stprod];
 
 if nargout == 2
     s = [];
